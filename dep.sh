aaa789a0a4d2dc49260c2c4487e0f9ce0e7c6576
siva1403 <k.s.krishnajob@gmail.com>
2025-12-24 14:43:56 +0530

    added script file

---
+++ dep.sh
@@ -0,0 +1,9 @@
+/!bin/bash
+sudo apt-get update
+sudo apt-get install git -y
+sudo apt-get install openjdk-11-jre -y
+sudo apt-get install maven -y
+git clone https://github.com/NarenPrakash/thymeleafExample.git
+cd thymeleafExample
+mvn clean install
+java -jar target/thymeleafExample-0.0.1-SNAPSHOT.jar &  
